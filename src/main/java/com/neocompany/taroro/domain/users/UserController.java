761fd5d32a993318c558da91488201e843b376aa
ggambo <ggambopce@gmail.com>
2026-03-04 19:44:56 +0900

    [REFACTOR] 로그아웃 서비스로직으로 리팩토링

--- src/main/java/com/neocompany/taroro/domain/users/UserController.java
+++ src/main/java/com/neocompany/taroro/domain/users/UserController.java
@@ -12,8 +12,6 @@ import com.neocompany.taroro.domain.users.dto.MeAuthResponseDto;
 import com.neocompany.taroro.domain.users.dto.WithdrawRequestDto;
 import com.neocompany.taroro.global.oauth2.PrincipalDetails;
 import com.neocompany.taroro.global.response.ApiResponse;
-import com.neocompany.taroro.global.sessions.SessionCookieUtil;
-import com.neocompany.taroro.global.sessions.SessionService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -26,7 +24,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    private final SessionService sessionService;
 
     @GetMapping("/me")
     public ResponseEntity<ApiResponse<MeAuthResponseDto>> me(@AuthenticationPrincipal PrincipalDetails principal) {
@@ -42,13 +39,7 @@ public class UserController {
     // 로그아웃
     @PostMapping("/logout")
     public ResponseEntity<ApiResponse<Void>> logout(HttpServletRequest req, HttpServletResponse res) {
-
-        String sid = SessionCookieUtil.readCookie(req, "SID");
-        // 1) DB 세션 삭제
-        sessionService.deleteSession(sid);
-        // 2) 쿠키 삭제(도메인/경로/secure/samesite 조합이 동일해야 잘 지워짐)
-        SessionCookieUtil.clearSidCookie(res, isHttps(req));
-
+        userService.logout(req, res);
         return ResponseEntity.ok(ApiResponse.ok("로그아웃 완료", null));
     }
 
@@ -66,10 +57,5 @@ public class UserController {
         userService.withdraw(user, password, request, response);
 
         return ResponseEntity.ok(ApiResponse.ok("회원탈퇴 완료", null));
-    }
-
-    private boolean isHttps(HttpServletRequest req) {
-        String proto = req.getHeader("X-Forwarded-Proto");
-        return "https".equalsIgnoreCase(proto) || req.isSecure();
     }
 }
